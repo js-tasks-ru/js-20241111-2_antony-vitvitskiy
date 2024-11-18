be019dc6aa70578f74d476e358d981bf2cea596c
Anton Vitvitskii <avitvitskii@flightpath3d.com>
2024-11-18 10:59:07 +0300

    2-3: Implements omit function

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,12 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+    let newObj = {};
+    Object.entries(obj).forEach((arr) => {
+        if (fields.indexOf(arr[0]) === -1) {
+            newObj[arr[0]] = arr[1];
+        }
+    });
 
+    return newObj;
 };
