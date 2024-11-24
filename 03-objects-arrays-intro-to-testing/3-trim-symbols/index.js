a19edb4468b78a8da5dd8b6d6fbba4957c8c2fca
Anton Vitvitskii <avitvitskii@flightpath3d.com>
2024-11-24 19:21:00 +0300

    3-3: Implements trimSymbols function

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,26 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    if (!string || size === 0) {
+        return "";
+    }
+    if (!size) {
+        return string;
+    }
+    
+    let formattedString = string[0];
+    let sameLetterCount = 1;
+    for(let i = 1; i < string.length; ++i) {
+      if (string[i] === formattedString[formattedString.length - 1]) {
+        if (sameLetterCount < size) {
+          formattedString += string[i];
+          sameLetterCount++;
+        }
+      } else {
+        formattedString += string[i];
+        sameLetterCount = 1;
+      }
+    }
 
+    return formattedString;
 }
