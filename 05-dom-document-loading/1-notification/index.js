20a78f868727b1fbf9db978e16f5f8ea52c0ead3
Anton Vitvitskii <avitvitskii@flightpath3d.com>
2024-12-02 12:23:09 +0300

    5-1: Implements NotificationMessage component

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,56 @@
 export default class NotificationMessage {
+    static lastShownNotification;
+    element;
+    message;
 
+    constructor(message, settings) {
+        this.message = message;
+        this.duration = settings?.duration;
+        this.type = settings?.type;
+        this.element = this.createElement(this.createTemplate());
+    }
+
+    createElement(template) {
+        const element = document.createElement('div');
+
+        element.innerHTML = template;
+
+        return element.firstElementChild;
+    }
+
+    createTemplate() {
+        return (`
+        <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+            <div class="timer"></div>
+            <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">
+                    ${this.message}
+                </div>
+            </div>
+        </div>
+        `);
+    }
+
+    show(targetElement) {
+        if (NotificationMessage.lastShownNotification) {
+            NotificationMessage.lastShownNotification.destroy();
+        }
+        NotificationMessage.lastShownNotification = this;
+
+        if (!!targetElement) {
+            targetElement.appendChild(this.element);
+            return;
+        }
+        document.body.appendChild(this.element);
+        this.remove();
+    }
+
+    remove() {
+        setTimeout(() => this.destroy(), this.duration);
+    }
+
+    destroy() {
+        this.element.remove();
+    }
 }
